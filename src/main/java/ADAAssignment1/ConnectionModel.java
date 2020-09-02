5f853cb25f515883b74fc207fffad727edfeff03
valar <valar@DESKTOP-IRTLSMT>
2020-09-02 14:29:57 +1200

    New Commit

---
+++ src/main/java/ADAAssignment1/ConnectionModel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ADAAssignment1;
+
+/**
+ *
+ * @author valar
+ */
+public class ConnectionModel {
+    
+    //Connection products
+    public static final String HOST_NAME = "localhost";
+    public static final int HOST_PORT = 6554;
+}
---
+++ src/main/java/ADAAssignment1/ConnectionObserver.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ADAAssignment1;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *
+ * @author valar
+ */
+public class ConnectionObserver extends Observable{
+    
+    
+}
